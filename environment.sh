bcb01844954864f978e2bd443576d4129d05ca93
Daniel Bisar <3024016+danielbisar@users.noreply.github.com>
2022-01-10 08:20:01 +0100

    bash: color test method

--- environment.sh
+++ environment.sh
@@ -9,3 +9,18 @@ fi
 # enable multi-select for FZF
 export FZF_DEFAULT_OPTS='-m'
 
+#### helpful functions ######
+function color_test()
+{
+    for fgbg in 38 48 ; do # Foreground / Background
+        for color in {0..255} ; do # Colors
+            # Display the color
+            printf "\e[${fgbg};5;%sm  %3s  \e[0m" $color $color
+            # Display 6 colors per lines
+            if [ $((($color + 1) % 6)) == 4 ] ; then
+                echo # New line
+            fi
+        done
+        echo # New line
+    done
+}
