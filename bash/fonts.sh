7b5de7e587678a26f5801ace4880d863ca76e86f
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-01-21 14:33:48 +0100

    corrected fonts and added kitty config

--- bash/fonts.sh
+++ bash/fonts.sh
@@ -1,12 +1,12 @@
 mkdir -p ~/.fonts
 pushd ~/.fonts
 
-wget https://github.com/adobe-fonts/source-code-pro/releases/download/2.038R-ro%2F1.058R-it%2F1.018R-VAR/VAR-source-code-var-1.018R.zip
-unzip VAR-source-code-var-1.018R.zip
+wget https://github.com/adobe-fonts/source-code-pro/releases/download/1.017R/source-code-pro-1.017R.zip
+unzip source-code-pro-1.017R.zip
 rm LICENSE.md
-rm VAR-source-code-var-1.018R.zip
+rm source-code-pro-1.017R.zip
+
 
 popd
-
 fc-cache -f -v
 
