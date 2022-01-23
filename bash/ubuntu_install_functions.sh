7bebd80cf49ad87193520bcd19e1a107e11c044c
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-01-23 11:58:19 +0100

    added some more install functions, integrated fonts.sh

--- bash/ubuntu_install_functions.sh
+++ bash/ubuntu_install_functions.sh
@@ -2,16 +2,26 @@
 
 install-basic-tools()
 {
+    # good to to after a fresh installation
     sudo apt update
-    sudo apt install fzf ripgrep
+    sudo apt upgrade
+
+    sudo apt install fzf ripgrep ppa-purge neovim
+
+    # python3 and pynvim
     sudo apt install python3 python3-pip
     pip3 install pynvim             # if not installed yet
     sudo pip3 install --upgrade pynvim   # upgrade if was already installed
 
-
+    # nodejs latest
     curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
-    sudo apt install nodejs npm
-    sudo npm -g install neovim    
+    sudo apt install nodejs 
+    sudo npm -g install neovim
+}
+
+install-basic-graphics-programs()
+{
+    sudo apt install mesa-utils
 }
 
 install-dotnet()
@@ -25,3 +35,53 @@ install-dotnet()
         sudo apt-get update && \
         sudo apt-get install -y dotnet-sdk-6.0
 }
+
+
+# fonts
+install-source-code-pro()
+{
+    mkdir -p ~/.fonts
+    pushd ~/.fonts
+
+    wget https://github.com/adobe-fonts/source-code-pro/releases/download/1.017R/source-code-pro-1.017R.zip
+    unzip source-code-pro-1.017R.zip
+    rm LICENSE.md
+    rm source-code-pro-1.017R.zip
+
+    popd
+    fc-cache -f -v
+}
+
+install-kitty-and-dependencies()
+{
+    sudo apt install fontconfig
+    install-source-code-pro
+    sudo apt install kitty imagemagick
+}
+
+
+# MESA
+install-latest-mesa()
+{
+    sudo add-apt-repository ppa:oibaf/graphics-drivers
+    sudo apt update
+    sudo apt upgrade
+}
+
+install-kiask-mesa()
+{
+    sudo add-apt-repository ppa:kisak/kisak-mesa
+    sudo apt update
+    sudo apt upgrade
+}
+
+remove-oibaf-ppa()
+{
+    sudo ppa-purge ppa:oibaf/graphics-driver
+}
+
+remove-kiask-ppa()
+{
+    sudo ppa-purge ppa:kisak/kisak-mesa
+}
+
