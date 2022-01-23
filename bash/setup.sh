df812507368b52090d71f0ad25981f867dd18a92
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-01-23 11:58:19 +0100

    added some more install functions, integrated fonts.sh

--- bash/setup.sh
+++ bash/setup.sh
@@ -1,28 +1,35 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-SETTINGS_BASE="$DIR"
+REPO_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+SETTINGS_BASE="$REPO_ROOT"
 
 if [[ ! -L ~/.vimrc ]]; then
     echo setting up .vimrc sym-link
     if [[ -f ~/.vimrc ]]; then
         mv ~/.vimrc ~/.vimrc.orignal
     fi
-    ln -s "$SETTINGS_BASE"/../vim/common.vim ~/.vimrc
+    ln -s "$SETTINGS_BASE"/vim/common.vim ~/.vimrc
 fi
 
 if [[ ! -L ~/.config/nvim/init.vim ]]; then
     echo setting up nvim init.vim symlink
     mkdir -p ~/.config/nvim
-    mv ~/.config/nvim/init.vim init.vim.original
-    ln -s "$SETTINGS_BASE"/../vim/common.vim ~/.config/nvim/init.vim
+    mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.original
+    ln -s "$SETTINGS_BASE"/vim/common.vim ~/.config/nvim/init.vim
 fi
 
-hasBashRcEnv=$(cat ~/.bashrc | grep '. $SETTINGS_BASE/environment.sh')
+if [[ ! -L ~/config/kitty/kitty.conf ]]; then
+    echo create kitty.conf symlink
+    mkdir -p ~/.config/kitty
+    mv ~/.config/kitty/kitty.conf ~/.config/kitty/kitty.conf.original
+    ln -s "$SETTINGS_BASE"/config/kitty/kitty.conf ~/.config/kitty/kitty.conf
+fi
+
+# TODO escape path for regex
+hasBashRcEnv=$(cat ~/.bashrc | grep ". $SETTINGS_BASE/bash/environment.sh")
 
 if [[ -z "$hasBashRcEnv" ]]; then
     echo "add env to bashrc"
-    echo ". $SETTINGS_BASE/environment.sh" >> ~/.bashrc
+    echo ". $SETTINGS_BASE/bash/environment.sh" >> ~/.bashrc
 fi
 
