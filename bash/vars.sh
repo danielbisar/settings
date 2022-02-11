d10bc0f55cb6e99dcc1e0163ccee9b735a31e2b9
Daniel Bisar <3024016+danielbisar@users.noreply.github.com>
2022-02-11 15:36:38 +0100

    Variables and installation scripts - wip

--- bash/vars.sh
+++ bash/vars.sh
@@ -1,5 +1,6 @@
-export DB_SETTINGS_BASE="$(dirname "${BASH_SOURCE[0]}")/../"
+export DB_SETTINGS_BASE="$(dirname "$BASH_SOURCE")/../"
 export DB_SETTINGS_VIM_BASE="$DB_SETTINGS_BASE/vim/"
 export DB_SETTINGS_BASH_BASE="$DB_SETTINGS_BASE/bash/"
 
+export DB_ROOT=~/.db/
 
