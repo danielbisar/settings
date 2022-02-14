0f42fd4c8b3a61799e69753ac9445553917c7d2e
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-02-14 21:55:00 +0100

    configure ale for vim correct to work with coc + change signs used in gutter

--- bash/vars.sh
+++ bash/vars.sh
@@ -1,6 +1,12 @@
-export DB_SETTINGS_BASE="$(realpath $(dirname "$BASH_SOURCE")/../)"
-export DB_SETTINGS_VIM_BASE="$DB_SETTINGS_BASE/vim/"
-export DB_SETTINGS_BASH_BASE="$DB_SETTINGS_BASE/bash/"
+#!/bin/bash
 
-export DB_ROOT=~/.db/
+DB_ROOT=~/.db/
+DB_SETTINGS_BASE="$(realpath "$(dirname "$BASH_SOURCE")"/../)"
+DB_SETTINGS_VIM_BASE="$DB_SETTINGS_BASE/vim/"
+DB_SETTINGS_BASH_BASE="$DB_SETTINGS_BASE/bash/"
+
+export DB_SETTINGS_BASE
+export DB_SETTINGS_VIM_BASE
+export DB_SETTINGS_BASH_BASE
+export DB_ROOT
 
