ddb6315ea82b2a504dc33c633b881704b295c4a3
Daniel Bisar <3024016+danielbisar@users.noreply.github.com>
2022-02-18 13:52:37 +0100

    drop kitty, add wezterm

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+
+-- https://wezfurlong.org/wezterm/config/lua/general.html
+return {
+    font = wezterm.font("SauceCodePro Nerd Font Mono"),
+    color_scheme = "Solarized Darcula",
+    audible_bell = "Disabled",
+}
