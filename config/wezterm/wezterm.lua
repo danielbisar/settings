8a828cfd5e96438dbfb519060ff13c66b7b7dbe3
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-02-18 16:26:47 +0100

    make wezterm.lua windows compatible

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,5 +5,8 @@ return {
     audible_bell = "Disabled",
     color_scheme = "Solarized Darcula",
     enable_scroll_bar = true,
-    font = wezterm.font("SauceCodePro Nerd Font Mono"),
+    font = wezterm.font_with_fallback({
+            "SauceCodePro Nerd Font Mono",
+            "SauceCodePro Nerd Font Mono Windows Compatible"
+        }),
 }
