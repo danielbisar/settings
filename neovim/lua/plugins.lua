55984eca768f28437b5c30a03f5fea00b2f03db2
Daniel Bisar <3024016+danielbisar@users.noreply.github.com>
2022-02-25 07:50:48 +0100

    nvim: reimplemented some settings with lua (F3=find file, F4=live grep)

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -2,68 +2,76 @@ local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
 end
 
 return require('packer').startup(function(use)
-  use {
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate | :TSInstall c_sharp | :TSInstall bash'
-  }
-
-  use {
-      'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
-
-  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
-
-  require('telescope').setup{
-      defaults = {
-        -- Default configuration for telescope goes here:
-        -- config_key = value,
-        -- ..
-      },
-      pickers = {
-        -- Default configuration for builtin pickers goes here:
-        -- picker_name = {
-        --   picker_config_key = value,
-        --   ...
-        -- }
-        -- Now the picker_config_key will be applied every time you call this
-        -- builtin picker
-      },
-      extensions = {
-         -- fzf = {
-         --     fuzzy = true,
-         --     override_generic_sorter = true,
-         --     override_file_sorter = true,
-         --     case_mode = "smart_case"
-         -- }
-      }
+    use { 'nvim-treesitter/nvim-treesitter', 
+          run = ':TSUpdate | :TSInstall c_sharp | :TSInstall bash'
     }
 
-  require('telescope').load_extension('fzf')
+    use { 'nvim-telescope/telescope.nvim',
+          requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
-  use { 'neoclide/coc.nvim', branch = 'release' }
+    use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use { 'dense-analysis/ale' }
+    use { 'neoclide/coc.nvim', branch = 'release' }
 
-  vim.cmd([[
-	let g:coc_global_extensions = [
-            \ 'coc-marketplace',
-            \ 'coc-clangd',
-            \ 'coc-cmake',
-            \ 'coc-git',
-            \ 'coc-lua',
-            \ 'coc-omnisharp',
-            \ 'coc-json',
-            \ 'coc-snippets',
-            \ 'coc-sql',
-            \ 'coc-xml',
-            \ 'coc-vimlsp' ]  ]])
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+
+    -- plugin settings
+    require('telescope').load_extension('fzf')
+    require('telescope').setup{
+        defaults = {
+            -- Default configuration for telescope goes here:
+            -- config_key = value,
+            -- ..
+        },
+        pickers = {
+            -- Default configuration for builtin pickers goes here:
+            -- picker_name = {
+            --   picker_config_key = value,
+            --   ...
+            -- }
+            -- Now the picker_config_key will be applied every time you call this
+            -- builtin picker
+        },
+        extensions = {
+            -- fzf = {
+            -- this are all default values
+            --     fuzzy = true,
+            --     override_generic_sorter = true,
+            --     override_file_sorter = true,
+            --     case_mode = "smart_case"
+            -- }
+        }
+    }
+
+    vim.cmd([[
+    let g:coc_global_extensions = [
+        \ 'coc-marketplace',
+        \ 'coc-clangd',
+        \ 'coc-cmake',
+        \ 'coc-git',
+        \ 'coc-lua',
+        \ 'coc-omnisharp',
+        \ 'coc-json',
+        \ 'coc-snippets',
+        \ 'coc-sql',
+        \ 'coc-xml',
+        \ 'coc-vimlsp' ]  ]])
+
+    vim.cmd([[
+        let ale_disable_lsp = 1
+        let ale_sign_error = ''
+        let ale_sign_warning = ''
+    ]])
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if packer_bootstrap then
+        require('packer').sync()
+    end
 end)
+
---
+++ neovim/lua/user_commands.lua
@@ -0,0 +1,3 @@
+--> this is going to come with nvim 0.7.0+
+-- vim.api.nvim_add_user_command('Settings', 'e ~/.config/nvim/init.lua');
+
