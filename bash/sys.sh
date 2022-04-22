5f83849b9f42b63285b36b263cbf95c7d89fa2b3
Daniel Bisar <daniel.bisar.extern@jungheinrich.de>
2022-04-22 16:32:23 +0200

    wip: network monitoring cli

--- bash/sys.sh
+++ bash/sys.sh
@@ -23,3 +23,28 @@ sys-file-provided-by()
 {
     dkpg -S "$1"
 }
+
+sys-stat-net-usage()
+{
+    # list network devices: ls -1 /sys/class/net/
+    local device="$1"
+    local duration=10
+
+    echo "sampling $duration seconds..."
+
+    orig_rx_bytes=$(cat /sys/class/net/$device/statistics/rx_bytes)
+    orig_tx_bytes=$(cat /sys/class/net/$device/statistics/tx_bytes)
+
+    sleep $duration
+
+    rx_bytes=$(cat /sys/class/net/$device/statistics/rx_bytes)
+    tx_bytes=$(cat /sys/class/net/$device/statistics/tx_bytes)
+
+    # 131072 = (1024*1024)/8 = mbit
+
+    rx_mbit_per_second=$(echo "scale=2; (($rx_bytes - $orig_rx_bytes) / $duration.0) / 131072.0" | bc -q)
+    tx_mbit_per_second=$(echo "scale=2; (($tx_bytes - $orig_tx_bytes) / $duration.0) / 131072.0" | bc -q)
+
+    echo recv: $rxb_per_second Mbit/s
+    echo send: $txb_per_second Mbit/s
+}
