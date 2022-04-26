fac41801b09654dc4475ac72bbacc2f14bd3e087
Daniel Bisar <3024016+danielbisar@users.noreply.github.com>
2022-04-26 21:49:18 +0200

    removed packer compiled

--- neovim/lua/plugins.lua
+++ neovim/lua/plugins.lua
@@ -6,7 +6,7 @@ if fn.empty(fn.glob(install_path)) > 0 then
 end
 
 return require('packer').startup(function(use)
-    use { 'nvim-treesitter/nvim-treesitter', 
+    use { 'nvim-treesitter/nvim-treesitter',
           run = ':TSUpdate | :TSInstall c_sharp | :TSInstall bash'
     }
 
@@ -18,10 +18,10 @@ return require('packer').startup(function(use)
     use { 'dense-analysis/ale' }
     use { 'neoclide/coc.nvim', branch = 'release' }
 
-    use {
-        'kyazdani42/nvim-tree.lua',
-        requires = { { 'kyazdani42/nvim-web-devicons' } }
-    }
+--    use {
+--        'kyazdani42/nvim-tree.lua',
+--        requires = { 'kyazdani42/nvim-web-devicons' }
+--    }
 
     use { 'mhartington/formatter.nvim' }
 
@@ -47,86 +47,118 @@ return require('packer').startup(function(use)
         augroup END
     ]], true)
 
-    require('nvim-tree').setup {
-        disable_netrw        = false,
-        hijack_netrw         = true,
-        open_on_setup        = false,
-        ignore_buffer_on_setup = false,
-        ignore_ft_on_setup   = {},
-        auto_close           = false,
-        auto_reload_on_write = true,
-        open_on_tab          = false,
-        hijack_cursor        = false,
-        update_cwd           = false,
-        hijack_unnamed_buffer_when_opening = false,
-        hijack_directories   = {
-            enable = true,
-            auto_open = true,
-        },
-        diagnostics = {
-            enable = false,
-            icons = {
-                hint = "",
-                info = "",
-                warning = "",
-                error = "",
-            }
-        },
-        update_focused_file = {
-            enable      = false,
-            update_cwd  = false,
-            ignore_list = {}
-        },
-        system_open = {
-            cmd  = nil,
-            args = {}
-        },
-        filters = {
-            dotfiles = false,
-            custom = {}
-        },
-        git = {
-            enable = true,
-            ignore = true,
-            timeout = 500,
-        },
-        view = {
-            width = 30,
-            height = 30,
-            hide_root_folder = false,
-            side = 'left',
-            preserve_window_proportions = false,
-            mappings = {
-                custom_only = false,
-                list = {}
-            },
-            number = false,
-            relativenumber = false,
-            signcolumn = "yes"
-        },
-        trash = {
-            cmd = "trash",
-            require_confirm = true
-        },
-        actions = {
-            change_dir = {
-                enable = true,
-                global = false,
-            },
-            open_file = {
-                quit_on_open = false,
-                resize_window = false,
-                window_picker = {
-                    enable = true,
-                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
-                    exclude = {
-                        filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame", },
-                        buftype  = { "nofile", "terminal", "help", },
-                    }
-                }
-            }
-        }
-    }
+--    require'nvim-tree'.setup { -- BEGIN_DEFAULT_OPTS
+--      auto_reload_on_write = true,
+--      disable_netrw = false,
+--      hide_root_folder = false,
+--      hijack_cursor = false,
+--      hijack_netrw = true,
+--      hijack_unnamed_buffer_when_opening = false,
+--      ignore_buffer_on_setup = false,
+--      open_on_setup = false,
+--      open_on_setup_file = false,
+--      open_on_tab = false,
+--      sort_by = "name",
+--      update_cwd = false,
+--      view = {
+--        width = 30,
+--        height = 30,
+--        side = "right",
+--        preserve_window_proportions = false,
+--        number = false,
+--        relativenumber = false,
+--        signcolumn = "yes",
+--        mappings = {
+--          custom_only = false,
+--          list = {
+--            -- user mappings go here
+--          },
+--        },
+--      },
+--      renderer = {
+--        indent_markers = {
+--          enable = false,
+--          icons = {
+--            corner = "└ ",
+--            edge = "│ ",
+--            none = "  ",
+--          },
+--        },
+--        icons = {
+--          webdev_colors = true,
+--        },
+--      },
+--      hijack_directories = {
+--        enable = true,
+--        auto_open = true,
+--      },
+--      update_focused_file = {
+--        enable = false,
+--        update_cwd = false,
+--        ignore_list = {},
+--      },
+--      ignore_ft_on_setup = {},
+--      system_open = {
+--        cmd = nil,
+--        args = {},
+--      },
+--      diagnostics = {
+--        enable = false,
+--        show_on_dirs = false,
+--        icons = {
+--          hint = "",
+--          info = "",
+--          warning = "",
+--          error = "",
+--        },
+--      },
+--      filters = {
+--        dotfiles = false,
+--        custom = {},
+--        exclude = {},
+--      },
+--      git = {
+--        enable = true,
+--        ignore = true,
+--        timeout = 400,
+--      },
+--      actions = {
+--        use_system_clipboard = true,
+--        change_dir = {
+--          enable = true,
+--          global = false,
+--          restrict_above_cwd = false,
+--        },
+--        open_file = {
+--          quit_on_open = false,
+--          resize_window = false,
+--          window_picker = {
+--            enable = true,
+--            chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+--            exclude = {
+--              filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+--              buftype = { "nofile", "terminal", "help" },
+--            },
+--          },
+--        },
+--      },
+--      trash = {
+--        cmd = "trash",
+--        require_confirm = true,
+--      },
+--      log = {
+--        enable = false,
+--        truncate = false,
+--        types = {
+--          all = false,
+--          config = false,
+--          copy_paste = false,
+--          diagnostics = false,
+--          git = false,
+--          profile = false,
+--        },
+--      },
+--    }
 
     -- plugin settings
     require('telescope').load_extension('fzf')
@@ -184,4 +216,3 @@ return require('packer').startup(function(use)
         require('packer').sync()
     end
 end)
-
