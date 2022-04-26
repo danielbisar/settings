f217c160a82f98df45974033e1fbd3cb884b1e47
Daniel Bisar <3024016+danielbisar@users.noreply.github.com>
2022-04-26 09:13:08 +0200

    bugfix in fix_bash_history.sh: history never got append, since wrong tail argument was used and history -a never called in exit hook

--- bash/fix_bash_history.sh
+++ bash/fix_bash_history.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 # history setup
 # don't put duplicate lines or lines starting with space in the history.
 # See bash(1) for more options
@@ -8,10 +8,11 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+
+# how much lines to remember in the running session
 HISTSIZE=10000
-HISTFILESIZE=20000
-
-
+# how much lines to save in the hist file
+HISTFILESIZE=10000
 
 # change behavior of bash history
 # normal behavior: bash overwrites the history -> last terminal wins
@@ -21,20 +22,35 @@ HISTFILESIZE=20000
 [[ -d /tmp/history ]] || mkdir --mode=0700 /tmp/history
 
 export DB_ORIGINAL_HISTFILE="$HISTFILE"
-export DB_ORIGINAL_HISTFILE_LINES=$(wc -l "$HISTFILE" | cut -d' ' -f1)
 HIST_FILE_NAME=/tmp/history/$(date +%Y_%m_%d_%H_%M_%S).$$
 
-# copy original file, so that after changing HISTFILE, we still have the old commands
-cp "$HISTFILE" "$HIST_FILE_NAME"
+
+# 'copy' history; but elimate duplicates (just the ones that follow up on each other); this is to clean up
+# history files that were used otherwise before
+cat "$HISTFILE" | uniq > "$HIST_FILE_NAME"
+export DB_ORIGINAL_HISTFILE_LINES=$(cat "$HIST_FILE_NAME" | wc -l)
 
 # update HISTFILE path
 HISTFILE="$HIST_FILE_NAME"
 
 function db-hist-on-close()
 {
-    # append the new lines to histfile
+    echo exit hook > ~/exit.log
+
+    # force write out of current history
+    history -a
+
+    LINES_TO_SKIP=$((DB_ORIGINAL_HISTFILE_LINES + 1))
+
+    # append the new lines to the original hist file
     # note: -n +NUM skips the first NUM lines
-    tail -n +$DB_ORIGINAL_HISTFILE_LINES "$HISTFILE" >> "$DB_ORIGINAL_HISTFILE"
+    tail -n +$LINES_TO_SKIP "$HISTFILE" | uniq >> "$DB_ORIGINAL_HISTFILE"
+
+
+    #echo "HISTFILE: $DB_ORIGINAL_HISTFILE" >> ~/exit.log
+    #echo "ORIG LENGTH: $DB_ORIGINAL_HISTFILE_LINES" >> ~/exit.log
+    #echo "tail -n +$LINES_TO_SKIP \"$HISTFILE\" | uniq" >> ~/exit.log
+    #tail -n +$LINES_TO_SKIP "$HISTFILE" | uniq >> ~/exit.log
 }
 
 trap db-hist-on-close EXIT
