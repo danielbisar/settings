4356e9514cf160b089bcd353422cc369bc24fdb3
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-05-08 13:55:18 +0200

    updated custom prompt and helpers for colors/chars

--- bash/colors.sh
+++ bash/colors.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
+db-output-reset()
+{
+    echo -en "\e[0m"
+}
+
+db-output-set-background-color()
+{
+    echo -en "\e[48;2;$1;$2;$3m"
+}
+
 db-colors-test-256()
 {
-    colors_per_line=19
+    columns=$(tput cols)
+    length_per_color=5
+
+    colors_per_line=$((columns / length_per_color))
 
     for color in {0..255}; do
-        # Display the color
-        printf "\e[48;5;%sm  %3s  \e[0m" $color $color
 
+        # for bright colors change foreground color to black, so you can still read the text
+        if [[ $color == 7 || $color == 15 ||
+            ($color -gt 37 && $color -lt 52) ||
+            ($color -gt 70 && $color -lt 88) ||
+            ($color -gt 95 && $color -lt 124) ||
+            ($color -gt 132 && $color -lt 232) ||
+            $color -gt 245 ]]; then
+            echo -n -e "\e[30m"
+        fi
+
+        # Display the color
+        printf "\e[48;5;%sm %3s " $color $color
+        db-output-reset
+
+        # new line after colors_per_line colors
         if [ $(((color + 1) % colors_per_line)) == 0 ]; then
             echo
         fi
@@ -15,90 +41,84 @@ db-colors-test-256()
     echo
 }
 
-db-reset-output()
-{
-    echo -en "\x1b[0m\n"
-}
-
-setBackgroundColor()
-{
-    #printf '\x1bPtmux;\x1b\x1b[48;2;%s;%s;%sm' $1 $2 $3
-    printf '\x1b[48;2;%s;%s;%sm' $1 $2 $3
-}
-
-# Gives a color $1/255 % along HSV
-# Who knows what happens when $1 is outside 0-255
-# Echoes "$red $green $blue" where
-# $red $green and $blue are integers
-# ranging between 0 and 255 inclusive
-rainbowColor()
-{
-    let h=$1/43
-    let f=$1-43*$h
-    let t=$f*255/43
-    let q=255-t
-
-    if [ $h -eq 0 ]; then
-        echo "255 $t 0"
-    elif [ $h -eq 1 ]; then
-        echo "$q 255 0"
-    elif [ $h -eq 2 ]; then
-        echo "0 255 $t"
-    elif [ $h -eq 3 ]; then
-        echo "0 $q 255"
-    elif [ $h -eq 4 ]; then
-        echo "$t 0 255"
-    elif [ $h -eq 5 ]; then
-        echo "255 0 $q"
-    else
-        # execution should never reach here
-        echo "0 0 0"
-    fi
-}
-
 db-colors-test-24bit()
 {
-    for i in $(seq 0 127); do
-        setBackgroundColor $i 0 0
-        echo -en " "
+    for gray in $(seq 0 3 255); do
+        db-output-set-background-color $gray $gray $gray
+        echo -n ' '
     done
-    resetOutput
-    for i in $(seq 255 -1 128); do
-        setBackgroundColor $i 0 0
-        echo -en " "
-    done
-    resetOutput
+    db-output-reset
+    echo
 
-    for i in $(seq 0 127); do
-        setBackgroundColor 0 $i 0
-        echo -n " "
+    for r in $(seq 0 3 255); do
+        db-output-set-background-color $r 0 0
+        echo -n ' '
     done
-    resetOutput
-    for i in $(seq 255 -1 128); do
-        setBackgroundColor 0 $i 0
-        echo -n " "
-    done
-    resetOutput
+    db-output-reset
+    echo
 
-    for i in $(seq 0 127); do
-        setBackgroundColor 0 0 $i
-        echo -n " "
+    for g in $(seq 0 3 255); do
+        db-output-set-background-color 0 $g 0
+        echo -n ' '
     done
-    resetOutput
-    for i in $(seq 255 -1 128); do
-        setBackgroundColor 0 0 $i
-        echo -n " "
-    done
-    resetOutput
+    db-output-reset
+    echo
 
-    for i in $(seq 0 127); do
-        setBackgroundColor $(rainbowColor $i)
-        echo -n " "
+    for b in $(seq 0 3 255); do
+        db-output-set-background-color 0 0 $b
+        echo -n ' '
     done
-    resetOutput
-    for i in $(seq 255 -1 128); do
-        setBackgroundColor $(rainbowColor $i)
-        echo -n " "
+    db-output-reset
+    echo
+
+    for m in $(seq 0 3 255); do
+        db-output-set-background-color $m $m 0
+        echo -n ' '
     done
-    resetOutput
+    db-output-reset
+    echo
+
+    for m in $(seq 0 3 255); do
+        db-output-set-background-color $m $((m / 2)) 0
+        echo -n ' '
+    done
+    db-output-reset
+    echo
+
+    for m in $(seq 0 3 255); do
+        db-output-set-background-color $m 0 $m
+        echo -n ' '
+    done
+    db-output-reset
+    echo
+
+    for m in $(seq 0 3 255); do
+        db-output-set-background-color $((m / 2)) 0 $m
+        echo -n ' '
+    done
+    db-output-reset
+    echo
+
+    for m in $(seq 0 3 255); do
+        db-output-set-background-color 0 $m $m
+        echo -n ' '
+    done
+    db-output-reset
+    echo
+
+    for m in $(seq 0 3 255); do
+        db-output-set-background-color 0 $((m / 2)) $m
+        echo -n ' '
+    done
+    db-output-reset
+    echo
+}
+
+db-color-tests()
+{
+    echo "test 256 color palette"
+    db-colors-test-256
+
+    echo "test 24 bit colors"
+    db-colors-test-24bit
 }
