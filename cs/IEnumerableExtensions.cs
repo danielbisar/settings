b1317a152a31d48b7e96c994d61d0c1c3099043f
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-05-24 20:53:35 +0200

    remove cs implementation of prompt

--- cs/IEnumerableExtensions.cs
+++
@@ -1,8 +0,0 @@
-public static class IListExtensions
-{
-    public static void AddRange<T>(this IList<T> list, IEnumerable<T> items)
-    {
-        foreach (var item in items)
-            list.Add(item);
-    }
-}
