0410d6a2246de4cea7b981bdc4f33eb6bb996451
Daniel Bişar <3024016+danielbisar@users.noreply.github.com>
2022-05-24 19:11:31 +0200

    separate classes into files

--- prompt/src/prompt.cpp
+++ prompt/src/prompt.cpp
@@ -8,10 +8,7 @@
 #include <sys/ioctl.h>
 #include <cstring>
 
-#include <git2/buffer.h>
-#include <git2/global.h>
-#include <git2/refs.h>
-#include <git2/repository.h>
+#include "git.hpp"
 
 // #define SET_FG(R, G, B) "\x1B[38;2;" #R ";" #G ";" #G "m"
 // #define SET_BG(R, G, B) "\x1B[48;2;" #R ";" #G ";" #G "m"
@@ -20,77 +17,6 @@
 using namespace std;
 
 // g++ -Wall -Wextra -o prompt -O2 ./prompt.cpp
-
-// https://libgit2.org/docs/guides/101-samples/
-class Git
-{
-private:
-    const string &_path;
-    const char *c_path;
-    bool _isGitRepo = false;
-    git_repository *_repo;
-
-public:
-    Git(const string &path)
-        : _path(path), c_path(path.c_str()), _repo(nullptr)
-    {
-        git_libgit2_init();
-
-        git_buf git_root = {NULL};
-        // git_repository repo = {NULL};
-
-        if (git_repository_discover(&git_root, c_path, 0, NULL) == 0)
-        {
-            _isGitRepo = true;
-
-            auto r = git_repository_open(&_repo, git_root.ptr);
-
-            if (r != 0)
-                cerr << "cannot open repo" << endl;
-        }
-        else
-            _isGitRepo = false;
-
-        // git_buf_free(&git_root);
-    }
-
-    // TODO clean up resources, even if the os takes care,
-    // when the program is closed it would be better to clean
-    // them for later reusage
-    // ~Git()
-    // {
-    //     git_libgit2_shutdown();
-    // }
-
-    bool is_git_repo()
-    {
-        return _isGitRepo;
-    }
-
-    bool is_clean()
-    {
-        return true;
-    }
-
-    const string get_current_branch_name()
-    {
-        // https://stackoverflow.com/questions/12132862/how-do-i-get-the-name-of-the-current-branch-in-libgit2
-        git_reference *head_ref;
-        git_reference_lookup(&head_ref, _repo, "HEAD");
-
-        const char *symbolic_ref;
-        symbolic_ref = git_reference_symbolic_target(head_ref);
-        std::string result;
-
-        // Skip leading "refs/heads/" -- 11 chars.
-        if (symbolic_ref)
-            result = &symbolic_ref[11];
-
-        git_reference_free(head_ref);
-
-        return result;
-    }
-};
 
 int main()
 {
